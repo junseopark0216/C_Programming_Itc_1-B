b175ffcc5c53b86d24440f2e1ae9458fe284b606
Junseo Park <128570773+junseopark0216@users.noreply.github.com>
2026-09-22 12:33:27 +0900

    Add files via upload

---
+++ week4/재귀.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void printNum (int n) {
+    if(n==0) {
+        return;
+    }
+    printNum(n-1);
+    printf("%d\n", n);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n );
+    printNum(n);
+    return 0;
+    
+}
